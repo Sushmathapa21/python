46ef6f81df20a9b450a194d15a1bc0ad102fdd60
sushmathapa21 <sushmathapa.3021@gmail.com>
2025-07-15 09:41:15 +0545

    save

--- 08_conditional.py
+++ 08_conditional.py
@@ -23,15 +23,57 @@
 
 # classwork:
 
-num1 = int(input("Enter number 1: "))
-num2 = int(input("Enter number 2: "))
-user_input = int(input("Enter '1' for addition\n Enter '2' for Subtraction\n Enter '3' for multiplication\n Enter '4' for division\n"))
-if user_input == 1:
-    print(f"The addition of num1 and num2 is {num1 + num2}")
-elif user_input == 2:
-    print(f"The subtraction of num1 and num2 is {num1 - num2}")
-elif user_input == 3:
-    print(f"The multiplication of num1 and num2 is {num1 * num2}")
-else:
-    print(f"The division of num1 and num2 is {num1 / num2}")
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+# user_input = int(input("Enter '1' for addition\n Enter '2' for Subtraction\n Enter '3' for multiplication\n Enter '4' for division\n"))
+# if user_input == 1:
+#     print(f"The addition of num1 and num2 is {num1 + num2}")
+# elif user_input == 2:
+#     print(f"The subtraction of num1 and num2 is {num1 - num2}")
+# elif user_input == 3:
+#     print(f"The multiplication of num1 and num2 is {num1 * num2}")
+# else:
+#     print(f"The division of num1 and num2 is {num1 / num2}")
+
+
+
+# x = 5
+# if x > 0 and x < 10:
+#     print("x is a positive single-digit number.")
+# elif x > 10 and x > 0:
+#     print("x is a positive number, but not single-digit.")
+# elif x == 0:
+#     print("x is zero.")
+# else:
+#     print("x is a negative number.")
+
+# user_input = int(input("Enter a Number: "))
+# if user_input > 0:
+#     print("The given number is positive: ", user_input)
+# elif user_input == 0:
+#     print("The number is zero: ", user_input)
+# else:
+#     print("The given number is negative: ", user_input)
+
+
+# user_input = int(input("Enter a Number: "))
+# if user_input%2 == 0:
+#     print("The given number is Even: ", user_input)
+# else:
+#     print("The given number is Odd: ", user_input)
+
+
+
+# user_input = int(input("Enter a Number: "))
+# if user_input > 0 and user_input % 2 == 0:
+#     print("The given number is a Positive Even number: ", user_input)
+# elif user_input > 0 and user_input % 2 != 0:
+#     print("The given number is a positive Odd number: ", user_input)
+# elif user_input < 0 and user_input % 2 == 0:
+#     print("The given number is a Negative Even number: ", user_input)
+# elif user_input < 0 and user_input % 2 != 0:
+#     print("The given number is a Negative Odd number: ", user_input)
+# else:
+#     print("The given number is zero which is a positive number: ", user_input)
+
 
